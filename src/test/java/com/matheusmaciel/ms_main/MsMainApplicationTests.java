036d610bb73c0e8fa1e62baa8ce0ddd4cc786d19
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-02-06 13:20:34 -0300

    Primeiro commit no novo repositório

---
+++ src/test/java/com/matheusmaciel/ms_main/MsMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmaciel.ms_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
